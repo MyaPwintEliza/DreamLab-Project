b4182c03cf2b7e1063179e7b19915831d88143c2
Florence Nway <112828449+nnway2022@users.noreply.github.com>
2023-03-02 17:01:00 +0000

    project set up

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="text-3xl font-bold underline">Dream Lab</div>;
+}
+
+export default App;
