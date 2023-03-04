6fe59467598d74b4a8345d707fa2af4a2f33c6cb
Thet Myoe Khaing <thetmyoekhaing.npi@gmail.com>
2023-03-05 04:12:33 +0630

    adding admin sidebar

--- src/layouts/admin/AdminLayout.jsx
+++ src/layouts/admin/AdminLayout.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+
+import Sidebar from "../../components/admin/Sidebar";
 
 function AdminLayout() {
-  return <div>AdminLayout</div>;
+  return (
+    <div>
+      <Sidebar />
+      <div>
+        <header>
+          <h1>Admin Dashboard</h1>
+        </header>
+        <div>
+          <div>
+            <Outlet />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default AdminLayout;
--- src/pages/admin/login/index.jsx
+++ src/pages/admin/login/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function index() {
-  return <div>Login</div>;
+  return (
+    <div>
+      <section className="flex flex-col">
+        <div className="bg-red-100">hello</div>
+        <div className="bg-dark"></div>
+      </section>
+    </div>
+  );
 }
 
 export default index;
