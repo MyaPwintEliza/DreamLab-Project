c4505edb425f91b2117de46f7b70f73416d7f263
Florence Nway <112828449+nnway2022@users.noreply.github.com>
2023-03-05 22:21:01 +0000

    update main to work properly after merge

--- src/components/admin/AdminContentTitle.jsx
+++ src/components/admin/AdminContentTitle.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { IoMdAddCircle } from "react-icons/io";
 
-const AdminContentTitle = () => {
+const AdminContentTitle = ({setCreateStatus}) => {
   return (
     <section className="flex justify-between items-center">
       <h3 className="font-semibold text-2xl">Plans</h3>
       <button
-        className="flex bg-dreamLabColor2 items-center py-2 px-10 gap-x-2"
+        className="flex bg-dreamLabColor2 items-center py-2 px-10 gap-x-2 rounded-md"
         onClick={() => setCreateStatus(true)}
       >
         <IoMdAddCircle />
--- src/pages/admin/plan/CreatePlan.jsx
+++ src/pages/admin/plan/CreatePlan.jsx
@@ -4,7 +4,7 @@ import React from "react";
 import { AiOutlineClose } from "react-icons/ai";
 
 import InputForm from "../../../components/form/InputForm";
-const CreatePlan = ({ createStatus }) => {
+const CreatePlan = ({ createStatus, setCreateStatus }) => {
   return (
     <div
       className={`fixed top-0 bg-white h-full text-black right-0 z-50 w-[500px] shadow-lg p-10 ${
@@ -13,7 +13,7 @@ const CreatePlan = ({ createStatus }) => {
     >
       <section className="flex justify-between items-center">
         <h2 className="font-semibold text-3xl">Create Plan</h2>
-        <AiOutlineClose size={25} />
+        <AiOutlineClose size={25} onClick={() => setCreateStatus(false)} className="cursor-pointer"/>
       </section>
       <form action="" className="flex flex-col gap-y-5 my-10">
         <InputForm title="Plan Code" name="code" placeholder="Type Plan Code" />
