f17738ca1c3d6a27988c5e1ee4e41f04b3dbbc92
Florence Nway <112828449+nnway2022@users.noreply.github.com>
2023-03-05 19:59:23 +0000

    clean up app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import Plan from "./pages/admin/plan";
 
 function App() {
-  // relative flex flex-auto flex-shrink-0 min-h-screen antialiased
-  return <div className="flex flex-auto flex-shrink-0" >
-    {/* fixed top-0 left-0 z-50 flex-col h-full w-14 md:w-14 lg:w-80 sidebar text-white text-opacity-50  */}
-    <aside className="h-full w-14 lg:w-80 bg-dreamLabColor4">
-      SideBar
-    </aside>
-    {/* <div text-black>Plan</div> */}
-    <Plan/>
-    </div>;
+  return (
+    <div className="flex flex-auto flex-shrink-0">
+      <aside className="h-full w-14 lg:w-80 bg-dreamLabColor4">SideBar</aside>
+      <Plan />
+    </div>
+  );
 }
 
 export default App;
