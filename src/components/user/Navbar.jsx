a719177216bf2ced0667fb23de8ef222b0d4d407
MyaPwintEliza <104987808+MyaPwintEliza@users.noreply.github.com>
2023-03-06 16:21:03 +0630

    user ui

---
+++ src/components/user/Navbar.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { AiFillHome } from "react-icons/ai";
+import { MdLocalLibrary } from "react-icons/md";
+import { TbMoneybag } from "react-icons/tb";
+import { AiOutlineSearch } from "react-icons/ai";
+import { BsPerson } from "react-icons/bs";
+
+function Navbar() {
+  return (
+    <div className="bg-white px-40 h-20 drop-shadow-xl w-full mt-5">
+      <nav className="flex ">
+        <img src="src/images/Icon.png" alt="Logo" />
+        <ul className=" mx-20 flex gap-10 mt-2 ">
+          <li className="flex gap-5">
+            <AiFillHome color="#0092FF" size={25} />
+            <p className="text-xl text-dreamLabColor1 no-underline">
+              <a href="#">Home</a>
+            </p>
+          </li>
+          <li className="flex gap-5">
+            <MdLocalLibrary color="#54595F" size={25} />
+            <p className="text-xl text-textColor1">
+              <a className="no-underline" href="#">
+                Library
+              </a>
+            </p>
+          </li>
+
+          <li className="flex gap-5">
+            <TbMoneybag color="#54595F" size={25} fill="#54595F" />
+            <p className="text-xl text-textColor1">
+              <a href="#">Pricing</a>
+            </p>
+          </li>
+        </ul>
+
+        <div className="no-underline">
+          <div className="absolute mx-5 mt-3.5">
+            <AiOutlineSearch size={20} color="#0092FF" />
+          </div>
+          <input
+            type="text"
+            className="bg-dreamLabColor5 text-sm h-10 w-80 px-12 rounded-full focus:outline-none hover:cursor-pointer"
+            placeholder="search here"
+            name=""
+          ></input>
+        </div>
+
+        <div>
+          <button class="bg-dreamLabColor2 h-9 w-32 hover:bg-blue-700 font-bold text-sm py-2 px-4 rounded mx-10">
+            <div className="absolute">
+              <BsPerson size={20} />
+            </div>
+            Login
+          </button>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
--- src/components/user/UserLogin.jsx
+++ src/components/user/UserLogin.jsx
@@ -1,7 +1,63 @@
 import React from "react";
+import { AiOutlineLine } from "react-icons/ai";
+import { AiFillGoogleCircle } from "react-icons/ai";
+import { IoMdClose } from "react-icons/io";
 
 function UserLogin() {
-  return <div>UserLogin</div>;
+  return (
+    <div className="py-5 px-10 w-[32rem] mx-auto mt-20 bg-white shadow-lg rounded-3xl ">
+      <div className="float-right">
+        <IoMdClose />
+      </div>
+      <section className="my-10 ">
+        <h3 className="text-center font-sans">Login</h3>
+        <p className="text-center font-medium text-lg py-2 text-textColor4  ">
+          Login to find new experiences
+        </p>
+        <form action="" className="flex flex-col gap-y-5 mt-8">
+          <input
+            type="email"
+            className="rounded-md py-1.5 px-4 border-stoke border-2 text-sm font-medium"
+            placeholder="Email / Phone no"
+          />
+          <input
+            id="password"
+            type="password"
+            className="rounded-md py-1.5 px-4 border-stoke border-2 text-sm font-medium"
+            placeholder="Password"
+          />
+          <a className="text-xs font-thin">Forget password?</a>
+          <button
+            className="bg-dreamLabColor2 text-dreamLabColor-1 rounded-md py-1.5 text-lg font-medium mt-3 
+            flex items-center justify-center gap-2"
+            type="submit"
+          >
+            Login
+          </button>
+          <div className="flex mx-auto">
+            <AiOutlineLine />
+            <p className="flex text-base font-medium mt-1">Or Sign in with</p>
+            <AiOutlineLine />
+          </div>
+          <button
+            className=" text-dreamLabColor4 border rounded-md py-1.5 text-lg font-thin w-48 mx-auto bg-transparent
+            flex items-center justify-center gap-2"
+            type="submit"
+          >
+            <AiFillGoogleCircle /> Google
+          </button>
+          <div>
+            <p className="text-sm font-thin text-center">
+              Don't have an account?&nbsp;&nbsp;
+              <span className="font-semibold text-dreamLabColor4">
+                Request Now
+              </span>
+            </p>
+          </div>
+        </form>
+      </section>
+    </div>
+  );
 }
 
 export default UserLogin;
