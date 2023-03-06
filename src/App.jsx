4a3efcfeaa22151892a873bd043973b1737d925f
MyaPwintEliza <104987808+MyaPwintEliza@users.noreply.github.com>
2023-03-06 16:30:41 +0630

    confit fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
 import { useState } from "react";
-<<<<<<< Updated upstream
+
 import { Navigate, Route, Routes, useRoutes } from "react-router-dom";
 import AdminRoutes from "./AdminRoutes";
-=======
-import Navbar from "./components/user/Navbar";
-import Footer from "./components/user/Footer";
-import UserLogin from "./components/user/UserLogin";
-import UserRegister from "./components/user/UserRegister";
->>>>>>> Stashed changes
 
 function App() {
   const AdminRouting = useRoutes(AdminRoutes);
   const [isAdmin, setIsAdmin] = useState(true);
 
   return (
-<<<<<<< Updated upstream
     <Routes>
       <Route
         path="/login"
@@ -26,17 +19,6 @@ function App() {
       />
       <Route path="/register" element={AdminRouting} />
     </Routes>
-=======
-    <div className="text-3xl font-bold underline">
-      <Navbar />
-
-      <UserLogin />
-
-      <UserRegister />
-
-      <Footer />
-    </div>
->>>>>>> Stashed changes
   );
 }
 
