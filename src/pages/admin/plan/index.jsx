ec33a83f08b110e090324bc4673ccb87536eac69
Florence Nway <112828449+nnway2022@users.noreply.github.com>
2023-03-12 23:11:11 +0000

    plan crud

--- src/pages/admin/plan/index.jsx
+++ src/pages/admin/plan/index.jsx
@@ -2,21 +2,72 @@ import React, { useState } from "react";
 import PlanItem from "./PlanItem";
 import CreatePlan from "./CreatePlan";
 import EditPlan from "./EditPlan";
-
+import { usePlanData } from "../../../hooks/usePlans";
 import DeletePlan from "./DeletePlan";
 import AdminContentTitle from "../../../components/admin/AdminContentTitle";
+import { ClipLoader } from "react-spinners";
 
 const index = () => {
   const [createStatus, setCreateStatus] = useState(false);
   const [editStatus, setEditStatus] = useState(false);
   const [deleteStatus, setDeleteStatus] = useState(false);
+  const [code, setCode] = useState("");
+  const [editPlan, setEditPlan] = useState({ code: "", name: "" });
+
+  const { isLoading, isError, error, data, refetch } = usePlanData();
+  console.log('data: ', data);
+
+  const refreshData = () => {
+    refetch();
+  };
+
+  if (isLoading) {
+    return (
+      <article className="flex items-center justify-center h-screen">
+        <ClipLoader />
+      </article>
+    );
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>;
+  }
+
   return (
-    <article className="w-full mx-5">
-      <AdminContentTitle setCreateStatus={setCreateStatus}/>
-      <PlanItem setEditStatus={setEditStatus} setDeleteStatus={setDeleteStatus} />
-      <CreatePlan createStatus={createStatus} setCreateStatus={setCreateStatus}/>
-      <EditPlan editStatus={editStatus} setEditStatus={setEditStatus}/>
-      <DeletePlan  deleteStatus={deleteStatus} setDeleteStatus={setDeleteStatus}/>
+    <article>
+      <AdminContentTitle
+        title="Plans List"
+        setCreateStatus={setCreateStatus}
+        createTitle="Create Plan"
+      />
+      <article className="my-10">
+        {data.map((plan) => (
+          <PlanItem
+            plan={plan}
+            setEditStatus={setEditStatus}
+            setDeleteStatus={setDeleteStatus}
+            setCode={setCode}
+            setEditPlan={setEditPlan}
+          />
+        ))}
+      </article>
+      <CreatePlan
+        createStatus={createStatus}
+        setCreateStatus={setCreateStatus}
+        refreshData={refreshData}
+      />
+      <EditPlan
+        editStatus={editStatus}
+        editPlan={editPlan}
+        setEditStatus={setEditStatus}
+        refreshData={refreshData}
+      />
+      <DeletePlan
+        code={code}
+        deleteStatus={deleteStatus}
+        setDeleteStatus={setDeleteStatus}
+        refreshData={refreshData}
+      />
     </article>
   );
 };
