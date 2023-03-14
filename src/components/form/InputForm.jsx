cc415aec83be1a4e145d53b16ff3d3d61e0d6847
Florence Nway <112828449+nnway2022@users.noreply.github.com>
2023-03-14 13:27:23 +0000

    input hook form

--- src/components/form/InputForm.jsx
+++ src/components/form/InputForm.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 const InputForm = ({
   title,
+  id,
   name,
   type = "text",
   placeholder = "",
@@ -14,7 +15,8 @@ const InputForm = ({
         {title}
       </label>
       <input
-        id={name}
+        id={id}
+        name={name}
         type={type}
         className="rounded-md py-1.5 px-4 border-stoke border-2 w-full"
         placeholder={placeholder}
