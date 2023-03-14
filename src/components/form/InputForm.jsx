bbeda8c08c89f79fffab88ff73cd4c5e17e279ec
Florence Nway <112828449+nnway2022@users.noreply.github.com>
2023-03-14 13:26:28 +0000

    input hook form

--- src/components/form/InputForm.jsx
+++ src/components/form/InputForm.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 
-const InputForm = ({ title, name, type = "text", placeholder = "" }) => {
+const InputForm = ({
+  title,
+  name,
+  type = "text",
+  placeholder = "",
+  register,
+  errors,
+}) => {
   return (
     <section className="w-full">
       <label htmlFor={name} className="font-semibold my-2 block">
@@ -11,7 +18,9 @@ const InputForm = ({ title, name, type = "text", placeholder = "" }) => {
         type={type}
         className="rounded-md py-1.5 px-4 border-stoke border-2 w-full"
         placeholder={placeholder}
+        register={register}
       />
+      {errors[name] && <p className="text-red-500">{errors[name]?.message}</p>}
     </section>
   );
 };
