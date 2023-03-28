52a8b2c59d0065d930a0b8ce3590a842126205f4
Florence Nway <112828449+nnway2022@users.noreply.github.com>
2023-03-28 13:36:29 +0100

    books added

--- src/AdminRoutes.jsx
+++ src/AdminRoutes.jsx
@@ -11,6 +11,7 @@ import Register from "./pages/admin/register";
 import ArticleAuthors from "./pages/admin/Authors/ArticleAuthors";
 import BookAuthors from "./pages/admin/Authors/BookAuthors";
 import CreateArticle from "./pages/admin/article/CreateArticle";
+import Books from "./pages/admin/Books";
 
 const AdminRoutes = [
   {
